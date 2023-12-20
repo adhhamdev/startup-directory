e3a983ef6613fcc22d1cd61766c5c0276a23c4b5
Adhham Safwan <msmadhhamg@gmail.com>
2023-12-20 12:37:36 +0000

    Add loading component

---
+++ app/loading.js
@@ -0,0 +1,11 @@
+const loading = () => {
+  return (
+    <div className="flex items-center justify-center h-screen">
+        <div className="text-5xl font-bold text-transparent bg-gradient-to-r bg-clip-text from-green-400 to-blue-500">
+            Loading...
+        </div>
+    </div>
+  );
+};
+
+export default loading;
