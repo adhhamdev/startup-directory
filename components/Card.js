4e40e42d2dfc89ff31d745493554fa5cac23c333
Adhham Safwan <msmadhhamg@gmail.com>
2024-09-01 16:22:58 +0530

    update deps

--- components/Card.js
+++ components/Card.js
@@ -7,10 +7,7 @@ import {
 const Card = ({ startup, handleModal }) => {
   return (
     <div
-      className="
-        flex flex-col justify-between bg-slate-200 shadow-lg rounded-lg px-4 py-4
-        hover:shadow-xl transition duration-200 ease-in-out
-        "
+      className="flex flex-col justify-between px-4 py-4 transition duration-200 ease-in-out rounded-lg shadow-lg  bg-slate-200 hover:shadow-xl"
     >
       <div>
         <h3 className="flex items-center text-xl font-medium text-gray-700">
@@ -21,13 +18,13 @@ const Card = ({ startup, handleModal }) => {
           <UserIcon className="w-6 pr-2" />
           {startup.InvestorsName}
         </p>
-        <p className="flex items-center text-gray-500 my-2">
+        <p className="flex items-center my-2 text-gray-500">
           <MapPinIcon className="w-6 pr-2" />
           {startup.CityLocation}
         </p>
       </div>
       <button
-        className="text-blue-500 font-bold hover:underline"
+        className="font-bold text-blue-500 hover:underline"
         onClick={() => handleModal(startup)}
       >
         Read more
